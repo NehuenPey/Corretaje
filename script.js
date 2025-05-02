b87a116b6834550c47ac8a75413a4d49c5965e45
nehuen <nehuenpeyrano@gmail.com>
2025-05-02 15:49:05 -0300

    Pequeña Lupita

--- script.js
+++ script.js
@@ -148,7 +148,31 @@ function configurarAccesibilidad() {
 
     aplicarTamañoFuente();
 }
-
+function abrirModal(guia) {
+    const modal = document.getElementById("modal");
+    const modalTitle = document.getElementById("modal-title");
+    const modalImg = document.getElementById("modal-img");
+    const modalDescription = document.getElementById("modal-description");
+  
+    // Cambiar la información del modal según el guía seleccionado
+    if (guia === 'guia1') {
+      modalTitle.innerText = "Lupita";
+    //   modalImg.src = "img/PeyranoNehuen.jpg";
+      modalDescription.innerText = "Descripción detallada del Guía 1... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe vitae explicabo obcaecati sint ea, veniam eveniet assumenda alias earum error esse id animi rerum? Aut, molestias. Quae nam maiores sunt?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi amet veritatis maxime unde animi in numquam ut laboriosam ullam nisi adipisci excepturi autem repudiandae, sapiente non nostrum suscipit beatae quo.";
+    } else if (guia === 'guia2') {
+      modalTitle.innerText = "Guía 2";
+      modalImg.src = "imagen-guia2.jpg";
+      modalDescription.innerText = "Descripción detallada del Guía 2...";
+    }
+  
+    modal.style.display = "block"; // Mostrar el modal
+  }
+  
+  function cerrarModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none"; // Cerrar el modal
+  }
+  
 
 // Inicialización
 document.addEventListener("DOMContentLoaded", () => {
